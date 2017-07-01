5b96803a728472bcc6074082bd966390895ddb48
Yoong Ti Lee <yoongtilee@Yoongs-MacBook-Pro.local>
2017-07-02 02:58:15 +0800

    hide or show projects and demo with button and list item

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 $(document).ready(function(){
 
-	$(".slick").slick({
-		infinite: true,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear'
-	});
+	
 
 	
-	$("#projects-toggle").on("click",function(){
-		
-	})
+	$("#project-toggle").on("click",function(){
+		console.log("Project");
+		//show projects section
+		if($("#projects").is(':visible')){
+			$("#projects").fadeOut();
+		}else{
+			//hide projects section if it is visible
+			if($("#demo").is(':visible')){
+				$("#demo").fadeOut(function(){
+					$("#projects").fadeIn();
+				})
+			}else{
+				$("#projects").fadeIn();
+			}
+		}
+	});
+
+	$(".video-game").on("click",function(){
+		//fade list out
+		$("#projects").fadeOut(function(){
+			//fade demo in
+			$("#demo").fadeIn();
+
+			$(".slick").slick({
+				infinite: true,
+				speed: 500,
+				fade: true,
+				cssEase: 'linear'
+			});
+		});
+	});
 
 });
