50afa9196d8c262ed82d9cfc1ec2ba64acdeeaf1
Erik Kee <erik.chkee@gmail.com>
2017-07-08 02:14:26 -0500

    Progress
    - Slicker occupies the whole window

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){			
 	$(".slick").slick({
 		infinite: true,
-		speed: 500,
+		speed: 200,
 		fade: true,
+		arrows: false,
 		cssEase: 'ease-out'
 	});
 
 	$("#demo").hide();
+
+
 
 /*	$(window).resize(function() {
         $('#master-container .slick-slide').height($('#master-container').height())
@@ -21,9 +24,11 @@ $(document).ready(function(){
 	$("#programming-toggle").on("click",function(){
 		console.log("Programming");
 		//show programming section
-		$("#demo").fadeOut(function(){
+		/*$("#demo").fadeOut(function(){
 				$("#programming").fadeIn();
-			});
+			});*/
+
+			$("#programming").fadeIn();
 
 		/*
 		if($("#programming").is(':visible')){
@@ -37,9 +42,25 @@ $(document).ready(function(){
 	});
 
 	$(".video-game").on("click",function(){
-		$("#programming").fadeOut(function(){
+
+		console.log("Video Game Card");
+
+		$(".slick").slick('slickFilter', "[name!='programming-list']");
+
+		/*$("#programming").fadeOut(function(){
 			$("#demo").fadeIn();
-		});
+		});*/
 	});
+
+
+	$('.custom-slick-prev').click(function(){
+	  $('.slick').slick('slickPrev');
+	  console.log("PREV");
+	})
+
+	$('.custom-slick-next').click(function(){
+	  $('.slick').slick('next');
+	  console.log("NEXT");
+	})
 
 });
