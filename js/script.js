7a5467d32b4e5082953128fbebd37e3ac4bbc0ba
Erik Kee <erik.chkee@gmail.com>
2017-08-18 23:55:10 -0500

    Mobile background tweak

--- js/script.js
+++ js/script.js
@@ -875,11 +875,11 @@ $(window).bind("load", function(){
 	if(!isMobileDevice()){
 		var video = document.getElementById('background-video');
 		var sourceMp4 = document.createElement('source');
-		sourceMp4.setAttribute('src', 'video/grass-35-10b.mp4');
+		sourceMp4.setAttribute('src', 'video/grass-35-10b121.mp4');
 		sourceMp4.setAttribute('type', 'video/mp4');
 
 		video.appendChild(sourceMp4);
-		video.play();
+		//video.play();
 
 		// If somehow the video still can't be played
 		if(video.paused === true){
@@ -890,7 +890,7 @@ $(window).bind("load", function(){
 
 			// Substitute by moving the background around 
 			$('#video-credit').hide();
-			$('#background-video-container').css("animation", "background-animationX 18s, background-animationY 12s");
+			$('#background-video-container').css("animation", "background-animationX 12s, background-animationY 8s");
 			$('#background-video-container').css("animation-timing-function", "ease-in-out");
 			$('#background-video-container').css("animation-iteration-count", "infinite");
 		}
